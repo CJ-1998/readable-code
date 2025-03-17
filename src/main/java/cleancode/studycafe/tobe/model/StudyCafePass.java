1d7031e301ea6227307a154899d9c55c304baa67
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-03-18 08:53:59 +0900

    Refactor : 강의 리팩토링 객체에 메시지 보내기
    Getter 대신 객체에 메시지 보내는 것으로 수정

--- src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
+++ src/main/java/cleancode/studycafe/tobe/model/StudyCafePass.java
@@ -18,6 +18,10 @@ public class StudyCafePass {
         return new StudyCafePass(passType, duration, price, discountRate);
     }
 
+    public boolean isSamePassType(StudyCafePassType passType) {
+        return this.passType == passType;
+    }
+
     public StudyCafePassType getPassType() {
         return passType;
     }
@@ -47,4 +51,13 @@ public class StudyCafePass {
         return "";
     }
 
+    public boolean isSameDurationType(StudyCafeLockerPass lockerPass) {
+        return lockerPass.isSamePassType(this.passType)
+                && lockerPass.isSameDuration(this.duration);
+    }
+
+    public boolean cannotUseLocker() {
+        return this.passType.isNotLockerType();
+        //return this.passType != StudyCafePassType.FIXED;
+    }
 }
