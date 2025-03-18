f0d10afcd06ab32d91033cd0ac88e93b18160224
CJ-1998 <choijun0627@konkuk.ac.kr>
2025-03-19 00:05:35 +0900

    Test : StudyCafePassOrderTest의 테스트 코드 작성
    StudyCafePassOrderTest의 총 금액 계산 로직 테스트 코드 작성

---
+++ src/test/java/cleancode/studycafe/tobe/model/order/StudyCafePassOrderTest.java
@@ -0,0 +1,47 @@
+package cleancode.studycafe.tobe.model.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import cleancode.studycafe.tobe.model.pass.StudyCafePassType;
+import cleancode.studycafe.tobe.model.pass.StudyCafeSeatPass;
+import cleancode.studycafe.tobe.model.pass.locker.StudyCafeLockerPass;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class StudyCafePassOrderTest {
+
+    @DisplayName("고정권이 아닌 패스의 금액은 패스의 금액과 할인율에 따라 계산된다.")
+    @Test
+    void calculateTotalPriceInNonFixedPass() {
+        //given
+        int passPrice = 4000;
+        StudyCafeSeatPass studyCafeHourlySeatPass = StudyCafeSeatPass.of(StudyCafePassType.HOURLY, 2, passPrice, 0.0);
+
+        StudyCafePassOrder passOrder = StudyCafePassOrder.of(studyCafeHourlySeatPass, null);
+
+        //when
+        int totalPrice = passOrder.getTotalPrice();
+
+        //then
+        assertThat(totalPrice).isEqualTo(passPrice);
+    }
+
+    @DisplayName("고정권이 아닌 패스의 금액은 패스의 금액과 할인율에 따라 계산된다.")
+    @Test
+    void calculateTotalPriceInFixedPass() {
+        //given
+        StudyCafeSeatPass studyCafeFixedSeatPass = StudyCafeSeatPass.of(StudyCafePassType.FIXED, 12, 700000, 0.15);
+        StudyCafeLockerPass studyCafeLockerPass = StudyCafeLockerPass.of(StudyCafePassType.FIXED, 4, 10000);
+
+        StudyCafePassOrder passOrder = StudyCafePassOrder.of(studyCafeFixedSeatPass, studyCafeLockerPass);
+
+        //when
+        int totalPrice = passOrder.getTotalPrice();
+
+        int answerPrice = 700000 + 10000 - (int) (700000 * 0.15);
+
+        //then
+        assertThat(totalPrice).isEqualTo(answerPrice);
+    }
+
+}
